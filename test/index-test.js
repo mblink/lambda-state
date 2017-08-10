43b6455fa06bc58500f7092b2d7bf98dcec39ad3
Matt Dziuban <mrdziuban@gmail.com>
2017-08-10 15:48:48 -0400

    Initial commit; add lambda state management.

---
+++ test/index-test.js
@@ -0,0 +1,81 @@
+import utils from './setup';
+import State from '../src/index';
+
+const logLevels = ['debug', 'info', 'warn', 'error'];
+const logFns = { debug: 'trace', info: 'log', warn: 'warn', error: 'error' };
+
+describe('State', () => {
+  beforeEach(() => State.init());
+
+  describe('::init', () => {
+    it('initializes a state object with an empty trace', () => expect(State.get().state.trace).to.deep.equal([]));
+  });
+
+  logLevels.forEach((level) => {
+    describe(`::${level}`, () => it(`adds a trace object with level set to ${level}`, () => {
+      State[level](`test ${level}`, { [`${level} key`]: `${level} value` })().then(() => {
+        const state = State.get().state;
+        expect(state.trace[0].level).to.equal(`${level}`);
+        expect(state.trace[0].message).to.equal(`test ${level}`);
+        expect(state.trace[0].aux).to.deep.equal({ [`${level} key`]: `${level} value` });
+      });
+    }));
+  });
+
+  describe('::finalize', () => {
+    describe('logging', () => {
+      beforeEach(() => { if (process.env.ALLOW_CONSOLE !== 'true') { console.log.restore(); } });
+
+      it('logs with `console.log` when the trace is empty', () => {
+        utils.stub(console, 'log');
+        State.finalize(() => {});
+        expect(console.log).to.have.been.calledOnce();
+      });
+
+      logLevels.forEach((level, i) => {
+        it(`logs with \`console.${logFns[level]}\` when the trace only has ${level} statements`, () => {
+          utils.stub(console, logFns[level]);
+          return State[level](level, {})().then(() => {
+            State.finalize(() => {});
+            expect(console[logFns[level]]).to.have.been.calledOnce();
+          });
+        });
+
+        it(`logs with \`console.${logFns[level]}\` when the trace has levels up to ${level}`, () => {
+          const levels = logLevels.slice(0, i + 1);
+          levels.forEach(l => utils.stub(console, logFns[l]));
+          return Promise.all(levels.map(l => State[l](l, {})())).then(() => {
+            State.finalize(() => {});
+            levels.slice(0, -1).forEach(l => expect(console[logFns[l]]).to.not.have.been.called());
+            expect(console[logFns[level]]).to.have.been.calledOnce();
+          });
+        });
+      });
+    });
+
+    describe('callback', () => {
+      logLevels.filter(l => l !== 'error').forEach((level, i) =>
+        it(`calls back with success when the level is <= ${level}`, () => {
+          const callback = utils.stub();
+          const levels = logLevels.slice(0, i + 1);
+          if (process.env.ALLOW_CONSOLE !== 'true') { console.log.restore(); }
+          levels.forEach(l => utils.stub(console, logFns[l]));
+          return Promise.all(levels.map(l => State[l](l, {})())).then(() => {
+            State.finalize(callback);
+            expect(callback).to.have.been.calledOnce();
+            expect(callback).to.have.been.calledWithExactly(null, utils.match.object);
+          });
+        }));
+
+      it('calls back with error when the level is error', () => {
+        const callback = utils.stub();
+        utils.stub(console, 'error');
+        return State.error('error', {})().then(() => {
+          State.finalize(callback);
+          expect(callback).to.have.been.calledOnce();
+          expect(callback).to.have.been.calledWithExactly(utils.match.object);
+        });
+      });
+    });
+  });
+});
---
+++ test/setup.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import sinon from 'sinon';
+
+global.expect = chai.expect;
+chai.use(require('dirty-chai'));
+chai.use(require('sinon-chai'));
+
+const utils = { match: sinon.match };
+const testFns = ['spy', 'stub', 'mock'];
+
+beforeEach(() => {
+  utils.sandbox = sinon.sandbox.create();
+  Object.assign(utils, ...testFns.map(fn => ({ [fn]: utils.sandbox[fn].bind(utils.sandbox) })));
+});
+
+afterEach(() => {
+  utils.sandbox.restore();
+  testFns.forEach(fn => delete utils[fn]);
+});
+
+export default utils;
